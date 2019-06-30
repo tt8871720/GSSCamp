e0dd13a4b87cf40820f9d7bf8a7209ad4ba6e470
tt8871720 <tt8871720@gmail.com>
2019-07-01 00:11:21 +0800

    restart,add

--- 6.29workshop3/js/script.js
+++ 6.29workshop3/js/script.js
@@ -1,16 +1,16 @@
 
 var bookDataFromLocalStorage = [];
 
-$(function(){
+$(function () {
     loadBookData();
     var data = [
-        {text:"資料庫",value:"image/database.jpg"},
-        {text:"網際網路",value:"image/internet.jpg"},
-        {text:"應用系統整合",value:"image/system.jpg"},
-        {text:"家庭保健",value:"image/home.jpg"},
-        {text:"語言",value:"image/language.jpg"},
-        {text:"行銷",value:"image/marketing.png"},
-        {text:"管理",value:"image/management.png"}
+        { text: "資料庫", value: "image/database.jpg" },
+        { text: "網際網路", value: "image/internet.jpg" },
+        { text: "應用系統整合", value: "image/system.jpg" },
+        { text: "家庭保健", value: "image/home.jpg" },
+        { text: "語言", value: "image/language.jpg" },
+        { text: "行銷", value: "image/marketing.png" },
+        { text: "管理", value: "image/management.png" }
 
     ]
     $("#book_category").kendoDropDownList({
@@ -27,7 +27,7 @@ $(function(){
             schema: {
                 model: {
                     fields: {
-                        BookId: {type:"int"},
+                        BookId: { type: "int" },
                         BookName: { type: "string" },
                         BookCategory: { type: "string" },
                         BookAuthor: { type: "string" },
@@ -36,6 +36,7 @@ $(function(){
                 }
             },
             pageSize: 20,
+            // filterable: true,
         },
         toolbar: kendo.template("<div class='book-grid-toolbar'><input class='book-grid-search' placeholder='依書籍名稱尋找......' type='text'></input></div>"),
         height: 550,
@@ -45,52 +46,102 @@ $(function(){
             numeric: false
         },
         columns: [
-            { field: "BookId", title: "書籍編號",width:"10%"},
-            { field: "BookName", title: "書籍名稱", width: "50%" },
-            { field: "BookCategory", title: "書籍種類", width: "10%" },
+            { field: "BookId", title: "書籍編號", width: "10%" },
+            { field: "BookName", title: "書籍名稱", width: "40%" },
+            { field: "BookCategory", title: "書籍種類", width: "20%" },
             { field: "BookAuthor", title: "作者", width: "15%" },
             { field: "BookBoughtDate", title: "購買日期", width: "15%" },
+            { field: "BookPublisher", title: "出版社", width: "20%" },
             { command: { text: "刪除", click: deleteBook }, title: " ", width: "120px" }
         ]
-        
+
     });
 
-//search
-$(".book-grid-search").on("input propertychange" , function(){
-    $("#book_grid").data("kendoGrid").dataSource.filter({
-        filters:[{
-            field: "BookName",
-            operator: "contains",
-            value: $(".book-grid-search").val()
-        }]
+    //search
+    $(".book-grid-search").on("input propertychange", function () {
+        $("#book_grid").data("kendoGrid").dataSource.filter({
+            filters: [{
+                field: "BookName",
+                operator: "contains",
+                value: $(".book-grid-search").val()
+            }]
+        });
+    });
+
+
+    //create new window
+    var add = $("#add");
+    var window = $("#addwindow");
+    add.click(function () {
+        window.data("kendoWindow").open();
     })
-})
-    
+    window.kendoWindow({
+        width: "1200px",
+        weigth: "250px",
+        height: "450px",
+        title: "add a new book",
+        visible: false,
+        //釘選,最大化,最小化,關閉
+        actions: [
+            "Pin",
+            "Minimize",
+            "Maximize",
+            "Close"]
+    }).data("kendoWindow").center();
+    //add
+    $("#sent").click(function () {
+        addBook();
+        kendo.alert("新增已成功");
+        $("#book_name").val("");
+        $("#book_author").val("");
+        $("#book_publisher").val("");
+    })
+    //window.data("kendoWindow").close();
+
 })
 
-function loadBookData(){
+function loadBookData() {
     bookDataFromLocalStorage = JSON.parse(localStorage.getItem("bookData"));
-    if(bookDataFromLocalStorage == null){
+    if (bookDataFromLocalStorage == null) {
         bookDataFromLocalStorage = bookData;
-        localStorage.setItem("bookData",JSON.stringify(bookDataFromLocalStorage));
+        localStorage.setItem("bookData", JSON.stringify(bookDataFromLocalStorage));
     }
 }
 
-function onChange(){
-    $(".book-image").attr("src" , $("#book_category").data("kendoDropDownList").value())
+function onChange() {
+    $(".book-image").attr("src", $("#book_category").data("kendoDropDownList").value())
 }
-  
-function deleteBook(e){
+
+function deleteBook(e) {
     console.log(e);
     var target = this.dataItem($(e.currentTarget).closest("tr"));
-   //查看目前目標 console.log(target);
-    for(var i = 0;i <= bookDataFromLocalStorage.length; i++){
-        if(bookDataFromLocalStorage[i].BookId == target.BookId){
-bookDataFromLocalStorage.splice(i,1);
-break;
+    //查看目前目標 console.log(target);
+    for (var i = 0; i <= bookDataFromLocalStorage.length; i++) {
+        if (bookDataFromLocalStorage[i].BookId == target.BookId) {
+            bookDataFromLocalStorage.splice(i, 1);
+            break;
         }
     }
     //update
-    localStorage.setItem("bookData" , JSON.stringify(bookDataFromLocalStorage));
+    localStorage.setItem("bookData", JSON.stringify(bookDataFromLocalStorage));
+    $("#book_grid").data("kendoGrid").dataSource.data(bookDataFromLocalStorage);
+}
+
+function addBook() {
+    const newdata = {
+
+        "BookId": bookDataFromLocalStorage[bookDataFromLocalStorage.length - 1].BookId + 1,
+        "BookCategory": $("#book_category").data("kendoDropDownList").text(),
+        "BookName": $("#book_name").val(),
+        "BookAuthor": $("#book_author").val(),
+        //"BookBoughDate": kendo.toString($("#bought_datepicker"), "yyyy-MM-dd"),
+        "BookBoughtDate": kendo.toString($("#bought_datepicker").data("kendoDatePicker").value(), "yyyy-MM-dd"),
+        "BookPublisher": $("#book_publisher").val()
+    }
+
+    //update
+    bookDataFromLocalStorage.push(newdata);
+    console.log(newdata);
+    localStorage.setItem("bookData", JSON.stringify(bookDataFromLocalStorage));
     $("#book_grid").data("kendoGrid").dataSource.data(bookDataFromLocalStorage);
 }
